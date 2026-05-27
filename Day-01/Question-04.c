f2ad500e87e0958f31705a59e4fc9a84d3c0fcb0
Aditya Mishra <emailadi77@gmail.com>
2026-05-27 18:54:34 +0530

    Added Question-04 in Day-01

---
+++ Day-01/Question-04.c
@@ -0,0 +1,15 @@
+//Question-04 : Write a program to Count digits in a number.
+#include <stdio.h>
+int main() 
+{
+    int n, count = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    while (n != 0) 
+    {
+        n /= 10;
+        count++;
+    }
+    printf("Number of digits in the entered number is: %d\n", count);
+    return 0;
+}
