f807e6f98893832fdeb2270e0e746b7982fd6850
Aditya Mishra <emailadi77@gmail.com>
2026-05-29 12:06:49 +0530

    Added Solutions for Day-07 Questions

---
+++ Day-07/Question-25.c
@@ -0,0 +1,21 @@
+//Question-25 : Write a program to Recursive factorial.
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if(n == 0)
+        return 1;
+    else
+        return n * factorial(n-1);
+}
+
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factorial of %d is %d\n", num, factorial(num));
+
+    return 0;
+}
