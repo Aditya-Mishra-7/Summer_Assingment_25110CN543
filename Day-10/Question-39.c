7ccae351c82a62f1110ba76321121f129fe55530
Aditya Mishra <emailadi77@gmail.com>
2026-06-09 08:18:47 +0530

    Added Solutions for Day-10 Questions

---
+++ Day-10/Question-39.c
@@ -0,0 +1,22 @@
+//Question-39 : Write a program to Print number pyramid.
+#include <stdio.h> 
+
+int main() 
+{
+    int n, i, j;
+    printf("Enter the number of rows for the number pyramid: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) 
+    {
+        for (j = 1; j <= n - i; j++) 
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++) 
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
