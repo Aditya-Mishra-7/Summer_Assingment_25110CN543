b5bfba3ca74a3336a3c529a93221fb059d1be968
Aditya Mishra <emailadi77@gmail.com>
2026-06-13 14:23:19 +0530

    Added Solutions for Day-13 Questions

---
+++ Day-13/Question-50.c
@@ -0,0 +1,26 @@
+//Question-50 : Write a program to find sum and average of array.
+#include <stdio.h>
+
+int main() 
+{
+    int arr[10], i, n;
+    int sum = 0;
+    float average;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the array: ");
+    for (i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    average = (float)sum / n;
+
+    printf("The sum of the array elements is: %d\n", sum);
+    printf("The average of the array elements is: %.2f\n", average);
+
+    return 0;
+}
