4a25da62d413462d77ce387aa86f6ab46507bed1
Aditya Mishra <emailadi77@gmail.com>
2026-06-16 17:02:17 +0530

    Added Solutions for Day-16 Questions

---
+++ Day-16/Question-64.c
@@ -0,0 +1,44 @@
+//Question-64 : Write a program to remove duplicates from an array.
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int newSize = 0;
+    for (int i = 0; i < n; i++) 
+    {
+        int isDuplicate = 0;
+        for (int j = 0; j < newSize; j++) 
+        {
+            if (arr[i] == arr[j]) 
+            {
+                isDuplicate = 1;
+                break;
+            }
+        }
+        if (!isDuplicate) 
+        {
+            arr[newSize] = arr[i];
+            newSize++;
+        }
+    }
+
+    printf("Array after removing duplicates: ");
+    for (int i = 0; i < newSize; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
