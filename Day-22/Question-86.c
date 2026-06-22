c1e2b42c8194d6b196bf3d61c3109b10fd723685
Aditya Mishra <emailadi77@gmail.com>
2026-06-22 21:17:09 +0530

    Added Solutions for Day-22 Questions

---
+++ Day-22/Question-86.c
@@ -0,0 +1,20 @@
+//Question-86 : Write a program to count words in a sentence.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[200];
+    int i, words = 1;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
+            words++;
+    }
+
+    printf("Number of words = %d\n", words);
+
+    return 0;
+}
